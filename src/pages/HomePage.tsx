7668e652928b9eb8eeb6036c86ab7180a3373107
AlonaRahilevych <ragilevic@gmail.com>
2026-02-17 17:06:08 +0100

    fix:loader imports

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,17 +3,17 @@ import { HeaderContent } from '@/widgets/dashboard/dashboard-header/ui/HeaderCon
 import { ProjectsOverview } from '@/widgets/dashboard/projects/home-overview/ui/ProjectsOverview';
 import { HomeStats } from '@/shared/ui/custom/Stats';
 import { HomeStatsNoProjects } from '@/widgets/dashboard/statistic/ui/HomeStatsNoProjects';
-import { Loader } from 'lucide-react';
 import { useParams } from 'react-router';
 import { useGetWorkspaceStats } from '@/features/workspace/model/useGetWorkspaceStats';
+import { Loader } from '@/shared/ui/Loader';
 
 export const HomePage = () => {
   const { id: workspaceId } = useParams<{ id: string }>();
   const { data: stats } = useGetWorkspaceStats(workspaceId!);
-  const { data: projects = [], isLoading } = useGetAllProjects(workspaceId!);
-  const isWorkspaceEmpty = !isLoading && projects.length === 0;
+  const { data: projects = [], isPending } = useGetAllProjects(workspaceId!);
+  const isWorkspaceEmpty = !isPending && projects.length === 0;
 
-  if (isLoading) return <Loader />;
+  if (isPending) return <Loader />;
 
   return (
     <div className=' grid grid-cols-1 py-2 space-y-6'>
