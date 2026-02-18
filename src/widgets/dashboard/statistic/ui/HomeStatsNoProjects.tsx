1a89b85b0ea945e9441afe54e3613027ec4f704d
AlonaRahilevych <ragilevic@gmail.com>
2026-02-18 18:30:25 +0100

    fix: add btn

--- src/widgets/dashboard/statistic/ui/HomeStatsNoProjects.tsx
+++ src/widgets/dashboard/statistic/ui/HomeStatsNoProjects.tsx
@@ -1,6 +1,27 @@
+import { useCreateProject } from '@/features/projects/model/useCreateProject';
+import {
+  ProjectForm,
+  type ProjectFormValues,
+} from '@/features/projects/ui/shared/ProjectForm';
 import { CustomButton } from '@/shared/ui/custom/CustomButton';
+import { Dialog, DialogContent } from '@/shared/ui/dialog';
+import { useState } from 'react';
+import { useParams } from 'react-router';
 
 export const HomeStatsNoProjects = () => {
+  const { id } = useParams<{ id: string }>();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { mutate, isPending } = useCreateProject();
+  const handleCreateProject = (data: ProjectFormValues) => {
+    mutate(
+      { dto: data, workspaceId: id! },
+      {
+        onSuccess: () => {
+          setIsModalOpen(false);
+        },
+      },
+    );
+  };
   return (
     <section className=' border rounded-md  p-8 text-center'>
       <h2 className='text-2xl font-medium mb-2'>
@@ -9,7 +30,24 @@ export const HomeStatsNoProjects = () => {
       <p className='text-muted-foreground mb-6'>
         Let's create your first project!
       </p>
-      <CustomButton label='Create your first project' className='mx-auto' />
+
+      <CustomButton
+        onClick={() => setIsModalOpen(true)}
+        label='Create your first project'
+        className='mx-auto'
+      />
+
+      <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DialogContent>
+          <ProjectForm
+            title='Create New Project'
+            submitText='Create'
+            isPending={isPending}
+            onSubmit={handleCreateProject}
+            onCancel={() => setIsModalOpen(false)}
+          />
+        </DialogContent>
+      </Dialog>
     </section>
   );
 };
